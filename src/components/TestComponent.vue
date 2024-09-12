8519e7e5f7db4332bd4e051f94fa278e730c58ce
sagesaka <sagesaka@riprice.co.jp>
2024-09-12 13:15:07 +0900

    コンポーネント操作を勉強

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,41 @@
+<!-- 練習 -->
+
+<!-- 親からデータを受け取る -->
+<script setup>
+const props = defineProps({
+  message: String
+})
+</script>
+
+<template>
+  {{ message }}
+</template>
+
+
+<!-- データバインディング -->
+
+<!-- イベントバインディング(@click="メソッド") -->
+<template>
+  <button @click="XButtonClick">ボタン</button>
+</template>
+
+<script setup>
+function XbuttonClick() {
+  console.log("クリックされました")
+}
+</script>
+
+<!-- 双方向バインディング -->
+<template>
+  <input type="text" id="name" v-model="name" />
+  <div>
+    入力された名前は: {{ name }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const name = ref("");
+</script>
+
+<!-- ref関数で値を宣言するとその値を常に監視し、データが変更されるとすぐに反映する。(DOMと仮想DOMの差分) -->
