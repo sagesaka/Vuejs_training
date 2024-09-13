adb88e0df02c295c5cfad5a4b3da9f3f72faa2f6
sagesaka <sagesaka@riprice.co.jp>
2024-09-13 17:53:37 +0900

    TodoApp.vueにコンポーネントを2つ配置

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="todoApp">
+    <TodoInput />
+    <TodoListView />
+  </div>
+</template>
+
+<script setup>
+import TodoInput from "./TodoInput.vue";
+import TodoListView from "./TodoListView.vue";
+</script>
