60be5fd3f9d8e6537a3e3b1b696841d584f5f24b
sagesaka <sagesaka@riprice.co.jp>
2024-09-17 13:20:23 +0900

    双方向バインディング

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,12 +2,19 @@
   <div>
     <form action="">
       <label>
-        やること<input type="text" />
+        やること<input type="text" v-model="input"/>
       </label>
       <label>
-        期限<input type="date" />
+        期限<input type="date" v-model="inputDate"/>
       </label>
       <input type="submit" value="登録" />
     </form>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const input = ref("");
+const inputDate = ref("");
+</script>
