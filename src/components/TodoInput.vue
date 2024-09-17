88f875882ed04b0e76e1def63aa2befa64d06e71
sagesaka <sagesaka@riprice.co.jp>
2024-09-17 13:15:28 +0900

    フォーム要素を作成

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,3 +1,13 @@
 <template>
-  
+  <div>
+    <form action="">
+      <label>
+        やること<input type="text" />
+      </label>
+      <label>
+        期限<input type="date" />
+      </label>
+      <input type="submit" value="登録" />
+    </form>
+  </div>
 </template>
