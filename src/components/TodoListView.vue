da5bbeede0f761c0527b5031af24df2e260812ea
sagesaka <sagesaka@riprice.co.jp>
2024-09-17 14:46:06 +0900

    タスク一覧ヘッダーの作成

--- src/components/TodoListView.vue
+++ src/components/TodoListView.vue
@@ -1,5 +1,16 @@
 <template>
-  
+  <div>
+    <table>
+      <tr>
+        <th class="th-id">ID</th>
+        <th class="th-value">やること</th>
+        <th class="th-limit">期限</th>
+        <th class="th-state">状態</th>
+        <th class="th-edit">編集</th>
+        <th class="th-delete">削除</th>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup>
