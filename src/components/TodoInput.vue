22e01b338f5369e1dbd8db319ff2463380da8331
sagesaka <sagesaka@riprice.co.jp>
2024-09-17 16:55:20 +0900

    完成

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,46 +1,54 @@
 <template>
-  <div>
-    <p v-if="isErrMsg">
-      必須項目を入力してください
-    </p>
-    <form @submit="onSubmitForm">
-      <label>
-        やること<input type="text" v-model="input"/>
-      </label>
-      <label>
-        期限<input type="date" v-model="inputDate"/>
-      </label>
-      <input type="submit" value="登録" />
+  <div class="input-area">
+    <form @submit.prevent="onClickBtn">
+      <label class="input-label"
+        >やること<input type="text" class="input" v-model="input" /></label
+      ><br />
+      <label class="input-label"
+        >期限<input type="date" class="input" v-model="inputDate" /></label
+      ><br />
+      <input type="submit" class="submit-btn btn" value="登録！" />
     </form>
   </div>
 </template>
-
 <script setup>
 import { ref } from "vue";
-import { statuses } from "@/const/status";
-
+import { defineEmits } from "vue";
 const input = ref("");
 const inputDate = ref("");
-
-const isErrMsg = ref(false);
-function onSubmitForm() {
-  if (input.value=="" || inputDate.value=="") {
-    isErrMsg.value = true;
-    event.preventDefault();
+const stateList = [
+  { id: 0, value: "未実施" },
+  { id: 1, value: "実行中" },
+  { id: 2, value: "完了" },
+];
+const emit = defineEmits(["SendErrMsg", "saveList"]);
+ 
+function onClickBtn() {
+  if (!input.value) {
+    emit("SendErrMsg", "やることを入力してください");
     return;
   }
+ 
+  emit("SendErrMsg", "");
+  //更新前に現在の状況を取得する
+ 
   const items = JSON.parse(localStorage.getItem("items")) || [];
-
-  const newItem = {
+ 
+  //期限は入力がなかった場合は空
+  items.push({
     id: items.length,
-    content: input.value,
-    limit: inputDate.value,
-    state: statuses.NOT_START,
+    value: input.value,
+    limit: inputDate.value || "",
+    state: stateList[0],
     onEdit: false,
-  };
-
-  items.push(newItem);
-
+  });
+  input.value = "";
+  saveList(items);
+}
+function saveList(items) {
   localStorage.setItem("items", JSON.stringify(items));
+  items = JSON.parse(localStorage.getItem("items"));
+  //テーブルの表示を更新するために親コンポーネントへイベントを送る
+  emit("saveList");
 }
 </script>
